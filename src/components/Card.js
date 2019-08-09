41c9ca292dc5d65d495e03f4970e5c0abbd43d1f
Rabbit <Corbineau@users.noreply.github.com>
2019-08-08 20:32:27 -0400

    refactored protfolio finally! Also added project 3

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,13 +6,13 @@ function Card(props) {
     return (
 
         <div className="foliocard">
-            <a href={appLink} target="_blank">
-                <img className="folioimg" src={img}
-                    alt={alt} />
+            <a href={props.appLink} rel="noopener noreferrer" target="_blank">
+                <img className="folioimg" src={props.img}
+                    alt={props.alt} />
             </a>
-            <div className="foliobanner">{appName}</div>
-            <p><a href={appLink}>Site</a> | <a
-                href={gitLink}>Github</a></p>
+            <div className="foliobanner">{props.appName}</div>
+            <p><a href={props.appLink || props.vidLink} rel="noopener noreferrer" target="_blank">{props.appLink ? "Site" : "Video"}</a> | <a
+                href={props.gitLink}>Github</a></p>
         </div>
     );
 }
