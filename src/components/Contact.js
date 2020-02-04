40e51862b6adbf9337c9d330c980935f2406a619
corbineau <lochinvar@gmail.com>
2020-02-03 22:04:08 -0500

    removing thing I didn't want there

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
                 </header>
                 <div className="bio">
                     <ContactForm />
-                    <h2 className="contactStats" id="stats">Email: lochinvar at gmail dot com | Phone: 919.599.7457</h2>
+                    <h2 className="contactStats" id="stats">Email: lochinvar at gmail dot com</h2>
                   
                         
                         <div className="response thankyou_message" id="thankyou_message">
