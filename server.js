72c09e9315efc4f58051ae497420cd7d7fd83215
corbineau <lochinvar@gmail.com>
2020-09-22 20:15:24 -0400

    cleaning up unneeded, commented out code

--- server.js
+++ server.js
@@ -6,16 +6,5 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'rabbitworks/build')));
 
-// An api endpoint that returns a short list of items
-// app.get('/api/getList', (req,res) => {
-//     var list = ["item1", "item2", "item3"];
-//     res.json(list);
-//     console.log('Sent list of items');
-// });
-
-// Handles any requests that don't match the ones above
-// app.get('*', (req,res) =>{
-    
-// });
 
 const port = process.env.PORT || 5000;
